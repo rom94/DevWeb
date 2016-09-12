1e078f6c627a33320eb867b706059a59a94e5a76
Romain De Page <romaindepage@gmail.com>
2016-09-12 13:55:28 +0200

    Ajout boucles, fonction, scope, arrays

--- Cours JS/script.js
+++ Cours JS/script.js
@@ -1,4 +1,4 @@
-//String
+/*//String
 var name = "Romain";
 console.log(name);
 
@@ -49,4 +49,68 @@ switch(variable){
     case 2:
         break;
     default:
+}*/
+
+
+/*//Boucles
+var number = 0;
+do{
+    console.log(number);
+    number++;
 }
+while(number > 0 && number < 3)
+    
+console.log("------------------------------------");
+    
+for(var x = 0; x < 5; x++){
+    console.log(x);
+}*/
+
+/*//Fonction
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+var a = 5;
+var b = 6;
+
+var result = multiply(a,b);
+console.log(result);*/
+
+/*//Scope
+function multiply(number1, number2){
+    resultMultiply =  number1 * number2;
+    return resultMultiply;
+}
+
+var a = 5;
+var b = 6;
+
+var result = multiply(a,b);
+console.log(resultMultiply);*/
+
+/*//Arrays
+var fruits = ["Pomme", "Banane", "Orange", "Citron"];
+console.log(fruits.length);
+
+for(var i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+fruits.push("Kiwi");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push("Kiwi");
+var agrumes = fruits.slice(2,4);
+console.log(agrumes);
+
+var agrumes = fruits.slice(2);
+console.log(agrumes);
+
+var myArray = [[0,1], [5,7,8], [12, 118]];
+console.log(myArray[2][1]);*/
+
+//Objects
