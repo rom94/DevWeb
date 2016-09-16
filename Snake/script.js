a16f64e4803350983fc9057690b087165db54ab6
Romain De Page <romaindepage@gmail.com>
2016-09-16 16:10:23 +0200

    Fin de la création du Snake et déplacement

--- Snake/script.js
+++ Snake/script.js
@@ -2,12 +2,10 @@ window.onload = function () {
     
     var canvasWidth = 900;
     var canvasHeight = 600;
-    var bool = true;
     var blockSize = 30;
     var ctx;
     var delay = 100;
-    var xCoord = 0;
-    var yCoord = 0;
+    var snakee;
     
     init();
     
@@ -18,25 +16,98 @@ window.onload = function () {
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        snakee = new Snake([[6, 4], [5, 4], [4, 4]], "right");
         refreshCanvas();
     }
     
     function refreshCanvas() {
-        xCoord += 5;
-        yCoord +=5;
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-        ctx.fillStyle = "#ff0000";
-        ctx.fillRect(xCoord, yCoord, 100, 50);
+        snakee.advance();
+        snakee.draw();
         setTimeout(refreshCanvas, delay);
     }
     
-    function Snake(body) {
+    function drawBlock(ctx, position){
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+    
+    function Snake(body, direction) {
         this.body = body;
+        this.direction = direction;
+        
         this.draw = function(){
             ctx.save();
             ctx.fillStyle = "#ff0000";
-            for();
+            for(var i = 0; i < this.body.length; i++){
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
         };
+        
+        this.advance = function(){
+            var nextPosition = this.body[0].slice();
+            switch(this.direction){
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw("Invalid Direction");
+            }
+            this.body.unshift(nextPosition);
+            this.body.pop();
+        };
+        
+        this.setDirection = function(newDirection){
+            var allowedDirections;
+            switch(this.direction){
+                case "left":
+                case "right":
+                    allowedDirections = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirections = ["left", "right"];
+                    break;
+                default:
+                    throw("Invalid Direction");
+            }
+            if(allowedDirections.indexOf(newDirection) > -1){
+                this.direction = newDirection;
+            }
+        };
+    }
+    
+    document.onkeydown = function handleKeyDown(e){
+        var key = e.keyCode;
+        var newDirection;
+        switch(key){
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+            default:
+                return;
+        }
+        snakee.setDirection(newDirection);
     }
     
 }
