38ccfb4a92d9d9e8ff57fd1e1d0f606c53e7afd8
Romain De Page <romaindepage@gmail.com>
2016-09-21 14:05:10 +0200

    Ajout jQuery/Html, jQuery/CSS et arborescence des éléments

--- Cours jQuery/script.js
+++ Cours jQuery/script.js
@@ -71,9 +71,139 @@ $(document).ready(function(){
         $('div').stop(true, true);
     });*/
 
-    $('#start').click(function() {
+    /*$('#start').click(function() {
         $('div').slideUp(3000).slideDown(3000).fadeOut(3000).fadeIn(3000);
+    });*/
+
+   /* $('#start').click(function() {
+        var myVar = $('#myInput').val();
+        console.log(myVar);
+    });*/
+
+    /*$('#start').click(function() {
+        var myVar = $('div').attr('style');
+        console.log(myVar);
+    });*/
+
+    /*$('#start').click(function() {
+        $('#p1').html('Voila mon <b>nouveau</b> texte');
+        $('#myInput').val("Nouvelle valeur");
+    });*/
+
+    /*$('#start').click(function() {
+        $('div').append('<p>Mon nouveau paragraphe </p>');
     });
+
+    $('#start').click(function() {
+        $('div').prepend('<p>Mon nouveau paragraphe </p>');
+    });
+
+    $('#start').click(function() {
+        $('div').after('<p>Mon nouveau paragraphe </p>');
+    });
+
+    $('#start').click(function() {
+        $('div').before('<p>Mon nouveau paragraphe </p>');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#p1').remove();
+    });
+
+    $('#start').click(function() {
+        $('#p1').empty();
+    });*/
+
+    /*$('#but1').click(function() {
+        $('#p1').addClass("red");
+    });*/
+
+
+    /*$('#but2').click(function() {
+        $('#p1').removeClass("red");
+    });*/
+
+    /*$('#but1').click(function() {
+        $('#p1').toggleClass('red');
+    });*/
+
+    /*$('#but1').click(function() {
+        $('#p1').css('color', 'red');
+    });*/
+
+    /*$('#but1').click(function() {
+        console.log($('#p1').css('color'));
+    });*/
+
+    /*$('#but1').click(function() {
+        $('#p1').css({
+            'color': 'red',
+            'font-weight': 'bold',
+            'background': 'yellow'
+        });
+    });*/
+
+    /*$('#but1').click(function() {
+        $('#but2').width(150);
+        $('#but2').height(150);
+    });*/
+
+    /*$('#start').click(function() {
+        $('#p1').css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#p1').parent().css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#p1').parents().css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#p1').parents('.special').css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#main').children().css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#main').find('p').css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#main').find('p').css('border-color', 'green');
+    });*/
+
+   /* $('#start').click(function() {
+        $('#p1').siblings().css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('#p1').next().css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('p').first().css('border-color', 'green');
+    });
+
+    $('#start').click(function() {
+        $('p').last().css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('p').eq(2).css('border-color', 'green');
+    });*/
+
+    /*$('#start').click(function() {
+        $('p').filter('.special').css('border-color', 'green');
+    });*/
+
+    $('#start').click(function() {
+        $('p').not('.special').css('border-color', 'green');
+    });
+
 
 
 });
