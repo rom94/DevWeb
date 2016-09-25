d6cddc97115ba36f39774294d05d5e507981cfda
Romain De Page <romaindepage@gmail.com>
2016-09-25 11:08:14 +0200

    Ajout des boutons et de l'action sur les boutons

--- SiteJQuery/script.js
+++ SiteJQuery/script.js
@@ -2,24 +2,34 @@ $(document).ready(function(){
 
 	var $mainMenuItems = $("#main-menu ul").children("li"),
 	totalMainMenuItems = $mainMenuItems.length,
-	openedIndex = -1,
+	openedIndex = 2,
 
 	init = function(){
+		bindEvent();
+		if (validIndex(openedIndex)) {
+			animateItem($mainMenuItems.eq(openedIndex), true, 700);
+		}
+	},
+
+	bindEvent = function(){
 		$mainMenuItems.children('.images').click(function() {
-			var newIndex = $(this).parent().index(),
-			$item = $mainMenuItems.eq(newIndex);
-			if (openedIndex === newIndex) {
-				animateItem($item, false, 250);
-				openedIndex = -1;
-			}
-			else{
-				if (validIndex(newIndex)) {
-					animateItem($mainMenuItems.eq(openedIndex), false, 250);
-					openedIndex = newIndex;
-					animateItem($item, true, 250);
-				}
-			}
+			var newIndex = $(this).parent().index();
+			checkAndAnimateItem(newIndex);
 		});
+
+		$('.button').hover(function(){
+			$(this).addClass('hovered');
+		},
+		function(){
+			$(this).removeClass('hovered');
+		});
+
+		$('.button').click(function() {
+			var newIndex = $(this).index();
+			checkAndAnimateItem(newIndex);
+		});
+
+
 	},
 
 	validIndex = function(indexToCheck){
@@ -33,6 +43,20 @@ $(document).ready(function(){
 
 		$colorImage.animate(colorImageParam,speed);
 		$item.animate(itemParam,speed);
+	},
+
+	checkAndAnimateItem = function (indexToCheckAndAnimate) {
+		if (openedIndex === indexToCheckAndAnimate) {
+			animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 250);
+			openedIndex = -1;
+		}
+		else{
+			if (validIndex(indexToCheckAndAnimate)) {
+				animateItem($mainMenuItems.eq(openedIndex), false, 250);
+				openedIndex = indexToCheckAndAnimate;
+				animateItem($mainMenuItems.eq(openedIndex), true, 250);
+			}
+		}
 	};
 
 	init();
